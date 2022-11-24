2ea2e9d9a54a826107326b707137d2806f72b44b
ibrahim.ozturk <ibrahim.ozturk@trendyol.com>
2022-11-25 01:27:42 +0300

    Convert oid to jsonb

--- src/test/java/com/example/oidtypeconversion/ApplicationTests.java
+++ src/test/java/com/example/oidtypeconversion/ApplicationTests.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.nio.charset.StandardCharsets;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -21,22 +19,20 @@ class ApplicationTests {
     private JdbcTemplate jdbcTemplate;
 
     @Test
-    void it_should_store_payload_as_oid() {
-        // when
-        SimpleCommand command = new SimpleCommand("test");
-        commandGateway.sendAndWait(command);
+    void it_should_store_payload_as_jsonb() {
+        // When
+        SimpleCommand simpleCommand = new SimpleCommand("test");
+        commandGateway.sendAndWait(simpleCommand);
 
-        // then
+        // Then
         String sql = "SELECT payload FROM domain_event_entry";
         jdbcTemplate.query(sql, rs -> {
             String columnType = rs.getMetaData().getColumnTypeName(1);
-            assertThat(columnType).isEqualToIgnoringCase("oid");
+            assertThat(columnType).isEqualToIgnoringCase("jsonb");
 
-            long oid = rs.getLong(1);
-            byte[] payload = jdbcTemplate.queryForObject("SELECT lo_get(?)", byte[].class, oid);
-            byte[] expected = "<com.example.oidtypeconversion.aggregate.SimpleEvent><id>test</id></com.example.oidtypeconversion.aggregate.SimpleEvent>"
-                    .getBytes(StandardCharsets.UTF_8);
-            assertThat(payload).isEqualTo(expected);
+            String value = rs.getString(1);
+            String expected = "{\"id\": \"test\"}";
+            assertThat(value).isEqualTo(expected);
         });
     }
 }
