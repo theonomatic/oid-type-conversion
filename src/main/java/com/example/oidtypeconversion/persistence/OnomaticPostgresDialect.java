ed291fcd78dda866a73c1d41ef8475a397ceb51b
misterdim <dimitrios.touloumis@onomatic.com>
2024-10-10 13:30:17 -0400

    Bytea Works

--- src/main/java/com/example/oidtypeconversion/persistence/OnomaticPostgresDialect.java
+++ src/main/java/com/example/oidtypeconversion/persistence/OnomaticPostgresDialect.java
@@ -13,7 +13,7 @@ public class OnomaticPostgresDialect extends PostgreSQLDialect {
     @Override
     protected String columnType(int sqlTypeCode) {
         return switch (sqlTypeCode) {
-            case SqlTypes.BLOB, SqlTypes.CLOB, SqlTypes.NCLOB -> "bytea";
+            case SqlTypes.BLOB/*, SqlTypes.CLOB, SqlTypes.NCLOB */-> "bytea";
             default -> super.columnType(sqlTypeCode);
         };
     }
